413b79254928d65ef087cd77e90798f798cda8b6
Fernando Vallejos <fer_pipi_2008@hotmail.com>
2022-05-20 19:59:23 -0300

    cambios en el Head

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
     <div>
       <Head>
         <meta charset="UTF-8"/>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         
         
